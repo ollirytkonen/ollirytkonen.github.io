9ef50d82f6d26c0cd424acc6a93784881174ca88
N8387 <n8387@student.jamk.fi>
2023-07-15 09:15:23 +0300

    mobile friendly

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const navSlide = () => {
 
 
     hamburger.addEventListener('click', () => {
-        console.log("clik");
+        
         nav.classList.toggle('nav-active');
     });
 
     navLinks.forEach( (link, index) =>{
-        console.log(index);
+        
         link.style.animation = 'navLinkFade 0.5 ease forwards' + index / 7 + 's';
     });
 }
