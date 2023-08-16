721a977417d4111b133818e6fc679c5ae2efcd38
N8387 <n8387@student.jamk.fi>
2023-08-16 17:31:15 +0300

    update

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#icon');
 
+const enableDarkMode = () => {
+    document.body.classList.add('dark-theme');
+    icon.src = "./images/light-mode.png";
+    localStorage.setItem('darkMode', 'enabled');
+
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-theme');
+    icon.src = "./images/dark-mode.png";
+    localStorage.setItem('darkMode', null);
+}
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if(darkMode !== 'enabled'){
+        enableDarkMode();
+        
+    }else{
+        disableDarkMode();
+        
+    }
+});
 
 function myFunction() {
 
